8ae0fb70bd8410eb2c371f709e04fc7fb85ac098
Martijn <martijnkragtwijk@gmx.com>
2018-04-06 00:19:49 +0200

    Added basic AnimationRecorder functionality

---
+++ DoAsIDo/Assets/Scripts/AnimationRecorder.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationRecorder : MonoBehaviour
+{
+
+    public RecordedSimpleAnimation recordingTarget;
+
+    public Transform transformToRecord;
+
+    public float startTime;
+    public float time;
+    float prevTime;
+
+    public bool startRecording = false;
+    public bool startPlaying = false;
+    public bool stop = false;
+    
+
+    public bool clear = false;
+    public bool cleanup = false;
+    [Range(1f, 60f)]
+    public float frameRate = 10f;
+    [Range(0.001f, 1f)]
+    public float epsilon = 0.01f;
+
+    public int numSamples = 0;
+    public bool showGizmo = true;
+
+    public enum MODE
+    {
+        STOPPED,
+        RECORDING,
+        PLAYING
+    }
+
+    public MODE mode;
+
+    void OnEnable()
+    {
+        Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+        SwitchModes();
+        switch (mode)
+        {
+            case MODE.STOPPED:
+                break;
+            case MODE.PLAYING:
+                time = Time.time - startTime;
+                if (time - prevTime > 1f / frameRate){
+                    recordingTarget.Play(transformToRecord, time);
+                    prevTime = time;
+                }
+                break;
+            case MODE.RECORDING:
+                time = Time.time - startTime;
+                if (time - prevTime > 1f / frameRate)
+                {
+                    recordingTarget.Record(transformToRecord, time);
+                    prevTime = time;
+                }
+                break;
+            default:
+                break;
+        }
+        numSamples = recordingTarget.timeStamps.Count;
+    }
+
+    void Stop()
+    {
+        time = 0f;
+        mode = MODE.STOPPED;
+    }
+
+    void StartPlaying()
+    {
+        startTime = Time.time;
+        time = 0f;
+        prevTime = -9999f;
+        mode = MODE.PLAYING;
+    }
+
+    void StartRecording()
+    {
+        startTime = Time.time;
+        time = 0f;
+        prevTime = -9999f;
+        mode = MODE.RECORDING;
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            startPlaying = true;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            stop = true;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            startRecording = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            clear = true;
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            cleanup = true;
+        }
+    }
+
+    void SwitchModes()
+    {
+        if (startPlaying)
+        {
+            if (mode == MODE.STOPPED)
+            {
+                StartPlaying();
+            }
+            startPlaying = false;
+        }
+        else if (stop)
+        {
+            if (mode != MODE.STOPPED)
+            {
+                Stop();
+            }
+            stop = false;
+        }
+        else if (startRecording)
+        {
+            if (mode == MODE.STOPPED)
+            {
+                StartRecording();
+            }
+            startRecording = false;
+        }
+        else if (clear)
+        {
+            recordingTarget.Clear();
+            clear = false;
+        }
+        else if (cleanup)
+        {
+            recordingTarget.Cleanup(epsilon);
+            cleanup = false;
+        }
+    }
+
+    void OnDrawGizmos() {
+        if (showGizmo && recordingTarget)
+        {
+            recordingTarget.DrawGizmos();
+        }
+    }
+}
---
+++ DoAsIDo/Assets/Scripts/RecordedSimpleAnimation.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="RecordedSimpleAnimation", menuName= "RecordedSimpleAnimation")]   
+public class RecordedSimpleAnimation : ScriptableObject {
+
+    [HideInInspector]
+    public List<Vector3> position;
+    [HideInInspector]
+    public List<float> timeStamps;
+
+    // helper
+    int lastIndex;
+
+    public void Clear()
+    {
+        position.Clear();
+        timeStamps.Clear();
+    }
+
+    public void Record(Transform trans, float time)
+    {
+        position.Add(trans.position);
+        timeStamps.Add(time);
+    }
+
+    // reset the playhead
+    public void Stop()
+    {
+        lastIndex = 0;
+    }
+
+    // find the indices in the arrays that surround a given timeStamp
+    // alpha is the interpolation value between 'before' and 'after' (in [0..1])
+    public void GetIndices(float time, out int before, out int after, out float alpha)
+    {
+        int i = 0;
+        int N = timeStamps.Count;
+        while (i < N && timeStamps[i] < time)
+        {
+            i++;
+        }
+        if (i < N)
+        {
+            before = i - 1;
+            after = i;
+            alpha = (time - timeStamps[before]) / (timeStamps[after] - timeStamps[before]);
+        } else {
+            // i==N
+            // time lies beyond any timeStamp in the list. Just return the last item:
+            before = N - 1;
+            after = N - 1;
+            alpha = 0f;
+        }
+    }
+
+    public void Play(Transform trans, float time)
+    {
+        int a;
+        int b;
+        float t;
+        if (position.Count > 0)
+        {
+            GetIndices(time, out a, out b, out t);
+            trans.position = Vector3.Lerp(position[a], position[b], t); // linearly interpolate
+        }
+    }
+
+    public void Cleanup(float epsilon)
+    {
+        float e2 = epsilon * epsilon;
+        for (int i = position.Count - 2; i > 1; i--)
+        { // leave the last sample intact
+            Vector3 posA = position[i - 1];
+            Vector3 posB = position[i];
+            if (Vector3.SqrMagnitude(posA - posB) < e2)
+            {
+                position.RemoveAt(i);
+                timeStamps.RemoveAt(i);
+            }
+        }
+    }
+
+    public void DrawGizmos()
+    {
+        for (int i = 0; i < position.Count - 1; i++) {
+            float time = timeStamps[i];
+            Gizmos.color = Color.Lerp(Color.black, Color.white, time - Mathf.Floor(time));
+            Gizmos.DrawLine(position[i], position[i + 1]);
+        }
+    }
+}
