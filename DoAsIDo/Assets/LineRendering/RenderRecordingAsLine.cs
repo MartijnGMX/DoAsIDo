1b27ca5658d57fb6d5aef31da9689f020c3a9da1
Martijn <martijnkragtwijk@gmx.com>
2018-04-06 14:06:49 +0200

    Experiments with lineRenderer and trailRenderer

---
+++ DoAsIDo/Assets/LineRendering/RenderRecordingAsLine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderRecordingAsLine : MonoBehaviour {
+    public LineRenderer lineRenderer;
+    public AnimationRecorder animationRecorder;
+
+	// Use this for initialization
+	void OnEnable () {
+        if (lineRenderer == null)
+        {
+            lineRenderer = GetComponent<LineRenderer>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // todo: optimize
+        Vector3[] pos = animationRecorder.recordingTarget.position.ToArray();
+        lineRenderer.SetPositions(pos);
+        lineRenderer.SetVertexCount(pos.Length);
+	}
+}
